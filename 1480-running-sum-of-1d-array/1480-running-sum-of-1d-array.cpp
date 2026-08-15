eab07edbb4f3cb747a117ebed0cb7b3823756df2
ADITYA SATI <satiaditya60@gmail.com>
2026-08-15 23:57:05 +0530

    Time: 0 ms (100%), Space: 12.7 MB (13.09%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) 
+    {
+        int sum = 0;
+
+        vector<int> result;
+
+        for(int i = 0; i< nums.size(); i++)
+        {
+            sum = sum + nums[i];
+            
+            result.push_back(sum);
+        }
+        
+        return result;
+    }
+};
