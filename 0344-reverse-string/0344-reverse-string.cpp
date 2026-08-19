05baae9ed3282574ae0a5769e9d3bcf6186c2c69
ADITYA SATI <satiaditya60@gmail.com>
2026-08-20 02:42:56 +0530

    Time: 155 ms (2%), Space: 27.3 MB (15.94%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i = 0;
-        int j = s.size() - 1;
-
-        while(i < j)
+        reverse(s.begin(),s.end());
+        for(int i = 0; i<s.size(); i++)
         {
-            swap(s[i], s[j]);
-            i++;
-            j--;
+            cout << s[i] << " ";
         }
     }
 };
