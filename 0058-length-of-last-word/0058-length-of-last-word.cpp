54bd7150b01b769038474c45ce68e76ee9f5f02d
ADITYA SATI <satiaditya60@gmail.com>
2026-09-21 23:57:43 +0530

    Time: 0 ms (100%), Space: 9 MB (8.49%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int c = 0;
+        int i = s.length()-1;
+
+        while(i>=0 && s[i] == ' ')
+        {
+            i--;
+        }
+        while(i>=0 && s[i] != ' ')
+        {
+            c++;
+            i--;
+        }
+
+        return c;
+        
+    }
+};
